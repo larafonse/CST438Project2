5e8844926409cbcde1d8066cdd535e4672b0b0d2
Dylan Hill <dylanrayhill@msn.com>
2021-10-17 15:46:54 -0700

    docker heroku working css working now too

--- src/pages/admin.py
+++ src/pages/admin.py
@@ -4,7 +4,7 @@ from .models import User, Item
 class wishListAdmin(admin.ModelAdmin):
     list_display = {'title', 'slug'}
 
-class userAdmin(admin.ModelAdmin):
+# class userAdmin(admin.ModelAdmin):
     list_display = {'firstName', 'lastName', 'customerId', }
     list_filter ={'firstName', }
     List_filter ={'customerId', }
@@ -14,7 +14,7 @@ class itemAdmin(admin.ModelAdmin):
     list_filter = {'customerId'}
 
 
-admin.site.register(User, userAdmin)
+# admin.site.register(User, userAdmin)
 admin.site.register(Item, itemAdmin)
 
 admin.site.site_header = "Wish List Admin"
