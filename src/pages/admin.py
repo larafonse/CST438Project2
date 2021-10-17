30806e682d2138ca95ae350019096d3c25795dec
DylanHill54 <dylanrayhill@msn.com>
2021-10-17 16:12:18 -0700

    docker heroku working css working now too

--- src/pages/admin.py
+++ src/pages/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import  Item
 
-admin.site.register(Item)
+
+
+
+class itemAdmin(admin.ModelAdmin):
+    list_display = {'productName', 'productDescription', }
+    list_filter = {'customerId'}
+
+
+admin.site.register(Item, itemAdmin)
+
+admin.site.site_header = "Wish List Admin"
